e433f9892a6e81ceeed949db9bbe1643b69fd367
kgil60 <gilvaryke@gmail.com>
2022-06-11 01:34:11 -0400

    Like model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User.js');
 const Post = require('./Post.js');
-const Comment = require('./Comment.js')
+const Comment = require('./Comment.js');
+const Like = require('./Like.js');
 
-module.exports = { User, Post, Comment };
+module.exports = { User, Post, Comment, Like };
